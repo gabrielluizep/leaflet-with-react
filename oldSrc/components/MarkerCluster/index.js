73bff3ba3df711a348c310a4fe7f74868cbd7d13
ga056303 <gabriel.pedro@intelbras.com.br>
2021-10-01 20:59:24 -0300

    wip: mocked v1 structure for consult

---
+++ oldSrc/components/MarkerCluster/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+import { makeStyles, Typography } from "@material-ui/core"
+
+import L from "leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import MarkerClusterGroup from "react-leaflet-markercluster"
+
+import { locations } from "../../locations"
+
+function MarkerCluster() {
+	return (
+		<MapContainer
+			style={{
+				width: "100vw",
+				height: "90vh"
+			}}
+			center={[-37.8871864333, 175.4691891]}
+			zoom={13}
+		>
+			<TileLayer
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+
+			<MarkerClusterGroup
+				iconCreateFunction={(cluster) =>
+					L.divIcon({
+						html: `<span>${cluster.getChildCount()}</span>`,
+						className: "clusterDivStyle",
+						iconSize: L.point(40, 40, true)
+					})
+				}
+			>
+				{locations.map((point, index) => {
+					return (
+						<Marker key={index} position={[point[0], point[1]]}>
+							<Popup>
+								<p>Latitude: {point[0]}</p>
+								<p>Longitude: {point[1]}</p>
+							</Popup>
+						</Marker>
+					)
+				})}
+			</MarkerClusterGroup>
+		</MapContainer>
+	)
+}
+
+export default MarkerCluster
---
+++ oldSrc/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import L from "leaflet"
+
+import App from "./App"
+
+import "./styles/index.css"
+import "leaflet/dist/leaflet.css"
+
+import icon from "leaflet/dist/images/marker-icon.png"
+import iconShadow from "leaflet/dist/images/marker-shadow.png"
+
+let DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
