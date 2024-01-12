b9dc31e422a9c7f8889f41a51663c5c808a29a8b
gabrielluizep <gabrielluizep@hotmail.com>
2024-01-11 21:22:11 -0300

    fix: moved to createRoot syntax

--- src/index.jsx
+++ src/index.jsx
@@ -18,9 +18,9 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+)
+
